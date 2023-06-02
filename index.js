d1b2b70341d62cea74f7d9bbc594afeae59eeb59
Rifat0001 <mahmudrifat764@gmail.com>
2023-06-02 17:15:10 +0600

    user added to db

--- index.js
+++ index.js
@@ -30,12 +30,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // for insert data in mongodb database 
+        const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+        // to get data from client site form 
+        app.post('/coffee', async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee)
+            // for send in mongodb / its next step to insert data 
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
